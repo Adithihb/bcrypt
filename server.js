8c65b0e68b509e3409b36fa17de5f82d6be0836b
= <=>
2023-11-22 16:42:02 +0530

    console log

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/', function (req, res) {
   });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err,hash)=> {
+    console.log(hash);
     bcrypt.compare(someOtherPlaintextPassword, hash, (err,res)=>{
         console.log(res);
     });
